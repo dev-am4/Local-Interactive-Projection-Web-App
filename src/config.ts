68ac4efc35619c8f087e25100a1d4fda5dcf2baa
dev-am4 <am4.production29@gmail.com>
2026-09-10 15:04:34 +0700

    Add kiosk timings and physical button key map

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+export const kioskConfig = {
+  idleResetMs: 22000,
+  skillStepMs: 2200,
+  careerRevealMs: 900,
+  safeAreaRatio: 4 / 3,
+  showPrototypeNotice: true,
+} as const
+
+export const keyboardMap: Record<string, number> = {
+  '1': 0,
+  Digit1: 0,
+  Numpad1: 0,
+  '2': 1,
+  Digit2: 1,
+  Numpad2: 1,
+  '3': 2,
+  Digit3: 2,
+  Numpad3: 2,
+  '4': 3,
+  Digit4: 3,
+  Numpad4: 3,
+  '5': 4,
+  Digit5: 4,
+  Numpad5: 4,
+  '6': 5,
+  Digit6: 5,
+  Numpad6: 5,
+}
