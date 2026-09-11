94dbcf1ae3ea3304a8beabe1f9a1f4cc59eba343
dev-am4 <am4.production29@gmail.com>
2026-09-11 18:52:06 +0700

    Enable astronaut green-spill cleanup at startup

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import '@fontsource/chakra-petch/500.css'
 import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
 import App from './App'
+import { installChromaKeyAssets } from './chroma-key-assets'
 import './styles.css'
 import './idle-home.css'
 import './idle-center-fix.css'
 import './career-alignment.css'
 import './career-layout-v3.css'
 import './netflix-font.css'
+
+installChromaKeyAssets()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
